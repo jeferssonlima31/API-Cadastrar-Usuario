133f70c25a7df495e46ad41937e583c00b5bda08
Jefersson Lima <jeferssonlima4@hotmail.com>
2024-11-14 12:23:44 -0300

    Test

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 from django.shortcuts import render
 
 # Create your views here.
